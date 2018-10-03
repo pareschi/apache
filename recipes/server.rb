674e250c7549784345deda8345c2a3b2deb506ef
root <root@ip-172-31-7-152.eu-west-1.compute.internal>
2018-10-03 23:08:56 +0000

    update index.html using ohai

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,7 +8,12 @@ package 'httpd' do
 end
 
 file '/var/www/html/index.html' do
-  content '<html><head></head><body><p align="center">My WebServer</p></body></html>'
+  content "<html><head></head><body><h1>My WebServer</h1>
+ HOSTNAME: #{node['hostname']}<br />
+ IPADDRESS: #{node['ipaddress']}<br />
+</body></html>
+
+"
 end
 
 service 'httpd' do
